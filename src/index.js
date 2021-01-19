3c6efcce8048ba0be2d1acfdc4da0a94a4b30d7e
Dmitry Uvarov <diman_niks@mail.ru>
2021-01-19 14:03:26 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strRemoveBrackets = str;
+ 	let foundBrackets = false;
+
+	bracketsConfig.forEach((arrBrackets) => {
+		let brackets = `${arrBrackets[0]}${arrBrackets[1]}`;
+
+		if (strRemoveBrackets.indexOf(brackets) !== -1) {
+			foundBrackets = true;
+			strRemoveBrackets = strRemoveBrackets.replace(brackets, '');
+			//console.log(brackets, strRemoveBrackets);
+		}
+	});
+
+	if (strRemoveBrackets && foundBrackets) return check(strRemoveBrackets, bracketsConfig);
+	if (strRemoveBrackets && !foundBrackets) return false;
+	if (!strRemoveBrackets) return true; 
 }
